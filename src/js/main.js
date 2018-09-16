6ca8c6c5898551fddd027cf88b2c5bb44872a097
SerAlex94 <oleksandrserhiienko1994@gmail.com>
2018-09-16 19:19:54 +0300

    some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,20 @@
-
+$('.item-days').click(function(){
+        $(this).toggleClass('active');
+        $('.form-field__slider').slideToggle(40);
+    });
 
 $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 1,
-      max: 24,
-      values: [ 6, 8 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "от " + ui.values[ 0 ] + " - до " + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "от " + $( "#slider-range" ).slider( "values", 0 ) +
-      " - до " + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
+  $( "#arrive" ).selectmenu();
+  $( "#departure" ).selectmenu();
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 1,
+    max: 24,
+    values: [ 6, 8 ],
+    slide: function( event, ui ) {
+      $( "#days" ).val( "от " + ui.values[ 0 ] + " - до " + ui.values[ 1 ] );
+    }
+  });
+  $( "#days" ).val( "от " + $( "#slider-range" ).slider( "values", 0 ) +
+    " - до " + $( "#slider-range" ).slider( "values", 1 ) );
+} );
